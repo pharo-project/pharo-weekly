2cc2e3eaf326203a0b675183ccf72588bfcd7b08
demarey <christophe.demarey@inria.fr>
2024-07-02 16:30:35 +0200

    use dev version of microdown

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 wget --quiet -O - get.pharo.org/64/120+vm | bash
-./pharo Pharo.image eval --save "Metacello new baseline: 'Microdown'; repository: 'github://pillar-markup/Microdown:master/src'; ignoreImage; onConflictUseIncoming; onWarning: [:ex | ex load]; load"
+./pharo Pharo.image eval --save "Metacello new baseline: 'Microdown'; repository: 'github://pillar-markup/Microdown:dev/src'; ignoreImage; onConflictUseIncoming; onWarning: [:ex | ex load]; load"
