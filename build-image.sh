61ce965ff45e81f591007f3d7a3f3cf8e4d4c071
StéphaneDucasse <stephane.ducasse@inria.fr>
2024-07-03 12:17:12 +0200

    build inage

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 wget --quiet -O - get.pharo.org/64/120+vm | bash
-./pharo Pharo.image eval --save "Metacello new baseline: 'Microdown'; repository: 'github://pillar-markup/Microdown:dev/src'; ignoreImage; onConflictUseIncoming; onWarning: [:ex | ex load]; load"
+./pharo Pharo.image eval --save "
+#( 'Microdown' 'BeautifulComments' 'DocumentBrowser' ) do: [ :name |
+		(IceRepository repositoryNamed: name)
+			ifNil: [ self inform: 'Project not found: ' , name ]
+			ifNotNil: [ :found |
+				found
+					unload;
+					forget ] ].
+Metacello new baseline: 'Microdown'; repository: 'github://pillar-markup/Microdown:dev/src'; ignoreImage; onConflictUseIncoming; onWarning: [:ex | ex load]; load"
